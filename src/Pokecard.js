d0de270d401d587ef51ed66137fea96fb0a05f27
Will Worth <will.worth@gmail.com>
2019-05-29 17:36:13 +0200

    card

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Pokecard.css'
 const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 class Pokecard extends Component {
@@ -9,7 +10,9 @@ class Pokecard extends Component {
         return (
             <div className="Pokecard">
                 <h1>{this.props.name }</h1>
-                <img src= {imgSrc}/>
+                <img src= {imgSrc} alt = {this.props.name}/>
+                <div>Type: {this.props.type}</div>
+                <div>EXP: {this.props.exp}</div>
             </div>
         )
 
